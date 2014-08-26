b69245faeab37c969c4cf8dee0e850a14a602467
lawshe <jelawshe@gmail.com>
2014-08-26 14:52:56 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,8 +1,10 @@
 Package.describe({
-  summary: 'Meteor smart package for FullPage.js. Create full screen pages fast and simple.'
+  summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
+  version: "1.0.1",
+  git: "https://github.com/lawshe/full-page.git"
 });
 
-Package.on_use(function (api) {
+Package.on_use(function (api, where) {
     api.add_files([
         'jquery.fullPage.css',
         'jquery.fullPage.min.js',
@@ -12,7 +14,11 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('full-page');
-  //api.add_files('full-page_tests.js', ['client', 'server']);
+  api.use('lawshe:full-page');
+    api.add_files([
+        'jquery.fullPage.css',
+        'jquery.fullPage.min.js',
+        'vendors/jquery.easings.min.js',
+        'vendors/jquery.slimscroll.min.js'
+    ], ['client', 'server']);
 });
-
