4992e63491dc7ef4b68a2fe412dd8e55ababd63b
lawshe <jelawshe@gmail.com>
2014-08-26 13:25:20 -0500

    smart package

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  summary: 'Meteor smart package for FullPage.js. Create full screen pages fast and simple.'
+});
+
+Package.on_use(function (api) {
+    api.add_files([
+        'jquery.fullPage.css',
+        'jquery.fullPage.min.js'
+    ], 'client');
+});
+
+Package.on_test(function (api) {
+  api.use('full-page');
+  //api.add_files('full-page_tests.js', ['client', 'server']);
+});
+
