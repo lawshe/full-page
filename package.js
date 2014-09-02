664151e92a8c14bd60351480753e65d7ac41395e
lawshe <jelawshe@gmail.com>
2014-09-02 17:45:56 -0500

    using meteor package for jQuery ui dependency

--- package.js
+++ package.js
@@ -1,14 +1,13 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.0.3",
+  version: "1.0.4",
   git: "https://github.com/lawshe/full-page.git"
 });
 
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0');
+  api.use('mizzao:jqueryui@1.11.0');
   api.addFiles('jquery.fullPage.css', 'client');
-  api.addFiles('vendors/jquery.easings.min.js', 'client');
-  api.addFiles('vendors/jquery.slimscroll.min.js', 'client');
   api.addFiles('jquery.fullPage.min.js','client');
 });
