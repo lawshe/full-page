053196286b36146da17b3475d3fdc20270ce63e9
lawshe <jelawshe@gmail.com>
2014-09-02 17:53:04 -0500

    new ui package

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.0.4",
+  version: "1.0.5",
   git: "https://github.com/lawshe/full-page.git"
 });
 
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0');
-  api.use('mizzao:jqueryui@1.11.0');
+  api.use('mrt:jquery-ui@1.9.2');
   api.addFiles('jquery.fullPage.css', 'client');
   api.addFiles('jquery.fullPage.min.js','client');
 });
