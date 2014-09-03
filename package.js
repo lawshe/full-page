9a6e6e80e62accdd6810ab482f44a687e4c8484a
lawshe <jelawshe@gmail.com>
2014-09-03 11:39:01 -0500

    removing jquery-ui package

--- package.js
+++ package.js
@@ -1,13 +1,17 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.0.8",
+  version: "1.0.9",
   git: "https://github.com/lawshe/full-page.git"
 });
 
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0');
-  api.use('mrt:jquery-ui@1.9.2');
+  //api.use('mrt:jquery-ui@1.9.2');
   api.addFiles('jquery.fullPage.css', 'client');
+
+
+  api.addFiles('vendors/jquery.easings.min.js', 'client');
+  api.addFiles('vendors/jquery.slimscroll.min.js', 'client');
   api.addFiles('jquery.fullPage.min.js','client');
 });
