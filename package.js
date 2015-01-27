4e84baa3c8aebea21f7badf0929b33edb5ed17b2
lawshe <jessica@jlawshe.com>
2015-01-27 10:44:57 -0600

    added slimscrolll

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.1.8",
+  version: "1.1.9",
   git: "https://github.com/lawshe/full-page.git"
 });
 
@@ -10,5 +10,6 @@ Package.on_use(function (api) {
   api.use('mizzao:jquery-ui@1.11.2');
   api.addFiles('jquery.fullPage.css', 'client');
   api.addFiles('vendors/jquery.easings.min.js','client');
+  api.addFiles('vendors/jquery.slimscroll.min.js','client');
   api.addFiles('jquery.fullPage.min.js','client');
 });
