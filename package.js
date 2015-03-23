29b2986998ab41a67d0a6c51016ae904fd904022
lawshe <jessica@jlawshe.com>
2015-03-23 10:37:19 -0500

    removed UI dependency

--- package.js
+++ package.js
@@ -1,15 +1,13 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.2.2",
+  version: "1.2.3",
   git: "https://github.com/lawshe/full-page.git"
 });
 
 
 Package.on_use(function (api) {
   api.use('jquery@1.0.0');
-  api.use('mizzao:jquery-ui@1.11.2');
   api.addFiles('jquery.fullPage.css', 'client');
-  api.addFiles('jquery.easings.min.js','client');
   api.addFiles('jquery.slimscroll.min.js','client');
   api.addFiles('jquery.fullPage.min.js','client');
 });
