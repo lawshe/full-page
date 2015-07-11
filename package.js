fcbfece954ce842e36119d27ba0e2f35c0db2edb
lawshe <jessica@jlawshe.com>
2015-07-11 09:28:07 -0500

    Updated plugin to 2.6.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.2.5",
+  version: "1.2.6",
   git: "https://github.com/lawshe/full-page.git"
 });
 
