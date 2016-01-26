23c56be28124276483bd6eadeea003b92082528d
lawshe <jessica@jlawshe.com>
2016-01-25 18:54:49 -0600

    FullPage.js version bump
    2.6.7 to 2.7.7

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.2.6",
+  version: "1.2.7",
   git: "https://github.com/lawshe/full-page.git"
 });
 
