b28b7955ded92d11c5d489e1470f4f7ce97aff15
lawshe <jessica@jlawshe.com>
2017-03-16 18:59:20 -0500

    Version Update
    Needed to republish on Atmosphere

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor smart package for FullPage.js. Create full screen pages fast and simple.",
-  version: "1.3.0",
+  version: "1.3.1",
   git: "https://github.com/lawshe/full-page.git"
 });
 
